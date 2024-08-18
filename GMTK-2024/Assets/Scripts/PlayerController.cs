67356dc02552238b72b2e5b3e5f205857073112e
kaiway-tang <86494513+kaiway-tang@users.noreply.github.com>
2024-08-17 23:33:55 -0700

    not sure

--- GMTK-2024/Assets/Scripts/PlayerController.cs
+++ GMTK-2024/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,16 @@ public class PlayerController : MobileEntity
     [SerializeField] GameObject[] gatlingBullets;
     [SerializeField] GameObject[] rockets;
 
+    [SerializeField] ParticleSystem tpPop;
+
     public int tier;
     public int type;
     const int GUNNER = 0, REAPER = 1, CONTROLLER = 2;
     public PlayerValues[] valRef;
+
+    [SerializeField] GameObject[] gunnerObjs;
+    [SerializeField] GameObject[] reaperObjs;
+    [SerializeField] GameObject[] controllerObjs;
 
     public static PlayerController self;
     // Start is called before the first frame update
@@ -40,11 +46,14 @@ public class PlayerController : MobileEntity
         HandleFacing();        
     }
 
+    #region MECH_TYPES
+
     void HandleSizeKeys()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            type = type == GUNNER ? REAPER : GUNNER;
+            if (type == GUNNER) { SetType(REAPER); }
+            else if (type == REAPER) { SetType(GUNNER); }
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
@@ -66,6 +75,50 @@ public class PlayerController : MobileEntity
             CameraManager.SetSize(valRef[tier].cameraSize);
         }
     }
+
+    void SetType(int newType)
+    {
+        if (type != newType)
+        {
+            if (type == GUNNER)
+            {
+                for (int i = 0; i < gunnerObjs.Length; i++)
+                {
+                    gunnerObjs[i].SetActive(false);
+                }
+            }
+
+            if (type == REAPER)
+            {
+                for (int i = 0; i < reaperObjs.Length; i++)
+                {
+                    reaperObjs[i].SetActive(false);
+                }
+            }
+
+
+
+            if (newType == GUNNER)
+            {
+                for (int i = 0; i < gunnerObjs.Length; i++)
+                {
+                    gunnerObjs[i].SetActive(true);
+                }
+            }
+
+            if (newType == REAPER)
+            {
+                for (int i = 0; i < reaperObjs.Length; i++)
+                {
+                    reaperObjs[i].SetActive(true);
+                }
+            }
+
+            type = newType;
+        }
+    }
+
+    #endregion
 
     #region AIMING
 
@@ -125,6 +178,7 @@ public class PlayerController : MobileEntity
                 if (type == GUNNER)
                 {
                     Instantiate(gatlingBullets[tier], firepointTrfm.position, firepointTrfm.rotation);
+                    CameraManager.SetTrauma(1);
                     primaryCD = 4;
                 }
                 else
@@ -145,7 +199,7 @@ public class PlayerController : MobileEntity
                 if (type == GUNNER)
                 {
                     Instantiate(rockets[tier], firepointTrfm.position, firepointTrfm.rotation);
-                    secondaryCD = 100;
+                    secondaryCD = 200;
                     secondaryTimer = 40;
                 }
                 else
@@ -159,6 +213,9 @@ public class PlayerController : MobileEntity
                     {
                         trfm.position += (mousePos - trfm.position).normalized * valRef[tier].blinkDistance;
                     }
+
+                    tpPop.Play();
+                    secondaryCD = 150;
                 }
             }
         }
